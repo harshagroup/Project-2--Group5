ae39d04590979e9945b2bf732c360aa021463bce
chandumukkera <chandukrishnareddy@gmail.com>
2016-08-08 17:51:43 -0500

    sprint 2

---
+++ database/HoursReport.java
@@ -0,0 +1,226 @@
+package invoice.screens;
+
+import invoice.beans.ApproveHours;
+import invoice.database.MyDatabaseHelper;
+import invoice.main.LoginHome;
+import invoice.main.SessionManager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class HoursReport extends JPanel {
+	private JButton logout;
+	private JButton help,company,clients,projects,employees,invoices,reports;
+	public HoursReport(final JFrame currentGUIFrame,final SessionManager sessionManager) {
+		setLayout(null);		
+		JLabel lblNewLabel = new JLabel("Welcome "+sessionManager.getUserName());
+		lblNewLabel.setBounds(350, 10, 150, 20);
+		currentGUIFrame.add(lblNewLabel);
+		
+		help = new JButton("Help");
+		help.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					
+			}
+		});
+		help.setBounds(610, 7, 75, 30);
+		currentGUIFrame.add(help);
+		logout = new JButton("Logout");
+		logout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentGUIFrame.getContentPane().removeAll();
+				currentGUIFrame.getContentPane().add(new LoginHome(currentGUIFrame));
+				currentGUIFrame.getContentPane().repaint();	
+			}
+		});
+		logout.setBounds(525, 7, 75, 30);
+		currentGUIFrame.add(logout);
+		lblNewLabel = new JLabel("Hours Report");
+		lblNewLabel.setBounds(300, 140, 200, 20);
+		currentGUIFrame.add(lblNewLabel);
+		
+		if(sessionManager.getUserRole()!=null && sessionManager.getUserRole().equalsIgnoreCase("ACCOUNTANT")){
+			company = new JButton("Company");
+			company.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new CompanyPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			company.setBounds(45, 100, 100, 30);
+			currentGUIFrame.add(company);
+			
+			clients = new JButton("Clients");
+			clients.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ClientsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			clients.setBounds(150, 100, 100, 30);
+			currentGUIFrame.add(clients);
+			
+			projects = new JButton("Projects");
+			projects.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ProjectsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			projects.setBounds(255, 100, 100, 30);
+			currentGUIFrame.add(projects);
+			
+			employees = new JButton("Employees");
+			employees.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new EmployeesPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			employees.setBounds(360, 100, 100, 30);
+			currentGUIFrame.add(employees);
+			
+			invoices = new JButton("Invoice");
+			invoices.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new InvoicesPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			invoices.setBounds(465, 100, 100, 30);
+			currentGUIFrame.add(invoices);
+			
+			reports = new JButton("Reports");
+			reports.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ReportsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			reports.setBounds(570, 100, 85, 30);
+			currentGUIFrame.add(reports);
+		}
+		if(sessionManager.getUserRole()!=null && sessionManager.getUserRole().equalsIgnoreCase("PROJECT MANAGER")){
+			projects = new JButton("Projects");
+			projects.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ProjectsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			projects.setBounds(75, 100, 90, 30);
+			currentGUIFrame.add(projects);
+			
+			employees = new JButton("Developers");
+			employees.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new EmployeesPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			employees.setBounds(185, 100, 100, 30);
+			currentGUIFrame.add(employees);
+			
+			invoices = new JButton("Approve Hours");
+			invoices.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ApprovedHours(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			invoices.setBounds(295, 100, 125, 30);
+			currentGUIFrame.add(invoices);
+			
+			reports = new JButton("Report");
+			reports.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ReportsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			reports.setBounds(430, 100, 100, 30);
+			currentGUIFrame.add(reports);
+		}
+		
+		if(sessionManager.getUserRole()!=null && sessionManager.getUserRole().equalsIgnoreCase("DEVELOPER")){
+			projects = new JButton("Projects");
+			projects.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ProjectsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			projects.setBounds(75, 100, 90, 30);
+			currentGUIFrame.add(projects);
+			
+			employees = new JButton("TimeSheet");
+			employees.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new TimeSheetPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			employees.setBounds(185, 100, 125, 30);
+			currentGUIFrame.add(employees);
+		}
+		
+		Object rowData[][] = { { "Row1-Column1", "Row1-Column2", "Row1-Column3", "Row1-Column4", "Row1-Column5"},
+                { "Row1-Column1", "Row1-Column2", "Row1-Column3", "Row1-Column4", "Row1-Column5"} };
+		Object columnNames[] = { "Project Number", "Developer Name", "Worked Date","Worked Hours","Status"};
+		final MyDatabaseHelper myDatabaseHelper=new MyDatabaseHelper();
+		final ArrayList approveList=myDatabaseHelper.getEmployeeReport();
+		if(approveList!=null &&  approveList.size()!=0){			
+		rowData=new String[ approveList.size()][5];
+		for(int count=0;count< approveList.size();count++){
+			ApproveHours approve=(ApproveHours) approveList.get(count);
+			int increment=0;
+			if(approve!=null){
+				rowData[count][increment]=""+approve.getProjectnumber();
+				increment=increment+1;
+				rowData[count][increment]=approve.getDevelopername();
+				increment=increment+1;
+				rowData[count][increment]=approve.getWorkdate();
+				increment=increment+1;
+				rowData[count][increment]=""+approve.getWorkhours();
+				increment=increment+1;
+				rowData[count][increment]=approve.getApprovestatus();
+			}				
+		}
+		}
+		
+		final JTable table = new JTable(rowData, columnNames);
+		table.setRowSelectionAllowed(true);
+		ListSelectionModel cellSelectionModel = table.getSelectionModel();
+		cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);		
+		JScrollPane scroll = new JScrollPane(table);
+		scroll.setBounds(50, 200, 600, 275);
+		currentGUIFrame.add(scroll);
+	}
+
+}
---
+++ database/ProjectsPanel.java
@@ -0,0 +1,301 @@
+package invoice.screens;
+
+import invoice.beans.Project;
+import invoice.database.MyDatabaseHelper;
+import invoice.main.LoginHome;
+import invoice.main.SessionManager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class ProjectsPanel extends JPanel {
+	private JButton logout;
+	private JButton help,company,clients,projects,employees,invoices,reports,editButton,deleteButton,addnewproject;
+	private String selectedIndex;
+	public ProjectsPanel(final JFrame currentGUIFrame,final SessionManager sessionManager) {
+		setLayout(null);		
+		JLabel lblNewLabel = new JLabel("Welcome "+sessionManager.getUserName());
+		lblNewLabel.setBounds(350, 10, 150, 20);
+		currentGUIFrame.add(lblNewLabel);
+		
+		help = new JButton("Help");
+		help.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					
+			}
+		});
+		help.setBounds(610, 7, 75, 30);
+		currentGUIFrame.add(help);
+		logout = new JButton("Logout");
+		logout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentGUIFrame.getContentPane().removeAll();
+				currentGUIFrame.getContentPane().add(new LoginHome(currentGUIFrame));
+				currentGUIFrame.getContentPane().repaint();	
+			}
+		});
+		logout.setBounds(525, 7, 75, 30);
+		currentGUIFrame.add(logout);
+		lblNewLabel = new JLabel("View Projects");
+		lblNewLabel.setBounds(300, 140, 200, 20);
+		currentGUIFrame.add(lblNewLabel);
+		
+		if(sessionManager.getUserRole()!=null && sessionManager.getUserRole().equalsIgnoreCase("ACCOUNTANT")){
+			company = new JButton("Company");
+			company.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new CompanyPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			company.setBounds(45, 100, 100, 30);
+			currentGUIFrame.add(company);
+			
+			clients = new JButton("Clients");
+			clients.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ClientsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			clients.setBounds(150, 100, 100, 30);
+			currentGUIFrame.add(clients);
+			
+			projects = new JButton("Projects");
+			projects.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ProjectsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			projects.setBounds(255, 100, 100, 30);
+			currentGUIFrame.add(projects);
+			
+			employees = new JButton("Employees");
+			employees.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new EmployeesPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			employees.setBounds(360, 100, 100, 30);
+			currentGUIFrame.add(employees);
+			
+			invoices = new JButton("Invoice");
+			invoices.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new InvoicesPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			invoices.setBounds(465, 100, 100, 30);
+			currentGUIFrame.add(invoices);
+			
+			reports = new JButton("Reports");
+			reports.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ReportsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			reports.setBounds(570, 100, 85, 30);
+			currentGUIFrame.add(reports);
+		}
+		if(sessionManager.getUserRole()!=null && sessionManager.getUserRole().equalsIgnoreCase("PROJECT MANAGER")){
+			projects = new JButton("Projects");
+			projects.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ProjectsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			projects.setBounds(75, 100, 90, 30);
+			currentGUIFrame.add(projects);
+			
+			employees = new JButton("Developers");
+			employees.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new EmployeesPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			employees.setBounds(185, 100, 100, 30);
+			currentGUIFrame.add(employees);
+			
+			invoices = new JButton("Approve Hours");
+			invoices.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ApprovedHours(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			invoices.setBounds(295, 100, 125, 30);
+			currentGUIFrame.add(invoices);
+			
+			reports = new JButton("Report");
+			reports.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ReportsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			reports.setBounds(430, 100, 100, 30);
+			currentGUIFrame.add(reports);
+		}
+		
+		if(sessionManager.getUserRole()!=null && sessionManager.getUserRole().equalsIgnoreCase("DEVELOPER")){
+			projects = new JButton("Projects");
+			projects.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new ProjectsPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			projects.setBounds(75, 100, 90, 30);
+			currentGUIFrame.add(projects);
+			
+			employees = new JButton("TimeSheet");
+			employees.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new TimeSheetPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			employees.setBounds(185, 100, 125, 30);
+			currentGUIFrame.add(employees);
+		}
+		
+		if(sessionManager.getUserRole()!=null && sessionManager.getUserRole().equalsIgnoreCase("ACCOUNTANT")){
+			addnewproject = new JButton("Add New Project");
+			addnewproject.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					currentGUIFrame.getContentPane().removeAll();
+					currentGUIFrame.getContentPane().add(new AddProjectPanel(currentGUIFrame,sessionManager));
+					currentGUIFrame.getContentPane().repaint();
+				}
+			});
+			addnewproject.setBounds(50, 160, 175, 30);
+			currentGUIFrame.add(addnewproject);
+		}
+		currentGUIFrame.setBounds(100,100,700,600);
+		Object rowData[][] = { { "Row1-Column1", "Row1-Column2", "Row1-Column3", "Row1-Column4", "Row1-Column5", "Row1-Column6", "Row1-Column7", "Row1-Column8", "Row1-Column9"},
+		                       { "Row1-Column1", "Row1-Column2", "Row1-Column3", "Row1-Column4", "Row1-Column5", "Row1-Column6", "Row1-Column7", "Row1-Column8", "Row1-Column9"} };
+		Object columnNames[] = { "Client", "Project Number", "Project Name", "State Date","End Date","Status","Project Manager","Client Contact","Budget"};
+		final MyDatabaseHelper myDatabaseHelper=new MyDatabaseHelper();
+		
+		ArrayList tempList=null;
+		if(sessionManager.getUserRole()!=null && sessionManager.getUserRole().equalsIgnoreCase("DEVELOPER")){
+			tempList=myDatabaseHelper.getProjects(sessionManager.getUserName());
+		}else{
+			tempList=myDatabaseHelper.getProjects();
+		}
+		final ArrayList projectList=tempList;
+		if( projectList!=null &&  projectList.size()!=0){			
+			rowData=new String[ projectList.size()][9];
+			for(int count=0;count< projectList.size();count++){
+				Project project=(Project) projectList.get(count);
+				int increment=0;
+				if(project!=null){
+					rowData[count][increment]=project.getClient();
+					increment=increment+1;
+					rowData[count][increment]=""+project.getProjectNumber();
+					increment=increment+1;
+					rowData[count][increment]=project.getProjectName();
+					increment=increment+1;
+					rowData[count][increment]=project.getStateDate();
+					increment=increment+1;
+					rowData[count][increment]=project.getEndDate();
+					increment=increment+1;
+					rowData[count][increment]=project.getStatus();
+					increment=increment+1;
+					rowData[count][increment]=project.getProjectManager();
+					increment=increment+1;
+					rowData[count][increment]=project.getClientContact();
+					increment=increment+1;
+					rowData[count][increment]=""+project.getBudget();
+				}				
+			}
+		}
+		
+		final JTable table = new JTable(rowData, columnNames);
+		table.setRowSelectionAllowed(true);
+	    ListSelectionModel cellSelectionModel = table.getSelectionModel();
+	    cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+	    cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
+	    	public void valueChanged(ListSelectionEvent e) {
+	    		String selectedData = null;
+		        if(table.getSelectedRow() > -1){
+		        	selectedIndex=""+table.getSelectedRow();		        	
+		        }
+	    	}
+	    });
+		JScrollPane scroll = new JScrollPane(table);
+		scroll.setBounds(50, 200, 600, 275);
+		currentGUIFrame.add(scroll);
+		
+		if(sessionManager.getUserRole()!=null && !(sessionManager.getUserRole().equalsIgnoreCase("DEVELOPER"))){
+			editButton = new JButton("Edit");
+			editButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if(selectedIndex!=null && selectedIndex.trim().length()!=0){
+						Project project=(Project)projectList.get(new Integer(selectedIndex).intValue());
+						currentGUIFrame.getContentPane().removeAll();
+						currentGUIFrame.getContentPane().add(new EditProjectPanel(currentGUIFrame,project,sessionManager));
+						currentGUIFrame.getContentPane().repaint();
+						selectedIndex="";
+					}else{
+						JOptionPane.showMessageDialog(editButton, "Please select one table row");
+					}
+				}
+			});
+			editButton.setBounds(50, 500, 100, 30);
+			currentGUIFrame.add(editButton);
+			
+			deleteButton = new JButton("InActive");
+			deleteButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if(selectedIndex!=null && selectedIndex.trim().length()!=0){
+						try{
+							Project project=(Project)projectList.get(new Integer(selectedIndex).intValue());
+							myDatabaseHelper.inactiveProject(project.getProjectNumber());
+							currentGUIFrame.getContentPane().removeAll();
+							currentGUIFrame.getContentPane().add(new ProjectsPanel(currentGUIFrame,sessionManager));
+							currentGUIFrame.getContentPane().repaint();
+						}catch(Exception ex){
+							ex.printStackTrace();
+						}
+					}else{
+						JOptionPane.showMessageDialog(editButton, "Please select one table row");
+					}
+				}
+			});
+			deleteButton.setBounds(165, 500, 100, 30);
+			currentGUIFrame.add(deleteButton);
+		}
+	}
+}
